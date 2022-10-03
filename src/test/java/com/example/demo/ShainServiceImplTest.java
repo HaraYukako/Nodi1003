6b4c3f87723af35bae599d8117d8d7a8c908ebc2
原 夕香子 <HARAY1@tss.co.jp>
2022-10-03 11:57:59 +0900

    3

--- src/test/java/com/example/demo/ShainServiceImplTest.java
+++ src/test/java/com/example/demo/ShainServiceImplTest.java
@@ -9,7 +9,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-public class ShainServiceImplTests {
+public class ShainServiceImplTest {
 
 	@MockBean
 	private ShainRepository shainRepository;
@@ -19,7 +19,7 @@ public class ShainServiceImplTests {
 		//shainRepositoryをモックにして、findByNoが機能するか？のテスト
 		when(shainRepository.selectByNo("100")).thenReturn("佐藤");
 		ShainService shainService = new ShainServiceImpl();
-		String employee = shainService.findByNo("200");
+		String employee = shainService.findByNo("100");
 		assertEquals(employee, "佐藤");
 	}
 
